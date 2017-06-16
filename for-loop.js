3428e515e3e90d11f4d878e45723af04d9d07b7d
Naricky <Na_ricky@hotmail.com>
2017-06-16 07:39:33 +0000

    initial commit with all my work so far

---
+++ for-loop.js
@@ -0,0 +1,9 @@
+var total;
+var total = 0;
+var limit;
+var limit = 10;
+var i;
+for (var i = 0 ; i < limit; i++){
+total += i;
+}
+console.log(total);
---
+++ rounding-numbers.js
@@ -0,0 +1,3 @@
+var roundUp;
+var roundUp = 1.5;
+Math.round(1.5);
